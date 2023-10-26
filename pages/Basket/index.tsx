9991b8a9edc6ac578c5352b01e01f86e2ca13c1f
Alireza <103531312+alirezashon@users.noreply.github.com>
2023-10-26 09:54:44 +0330

    start with post box me

--- pages/Basket/index.tsx
+++ pages/Basket/index.tsx
@@ -1,28 +1,51 @@
-/** @format */
 
-import { useState } from 'react'
-import styles from './index.module.css'
-import { AiOutlineShoppingCart } from 'react-icons/ai'
+// import { useState } from 'react'
+// import styles from './index.module.css'
+// import { AiOutlineShoppingCart } from 'react-icons/ai'
 
-const Basket = () => {
-	const [isBasketOpen, setIsBasketOpen] = useState<boolean>(false)
-	return (
-		<>
-			{isBasketOpen ? (
-                <div>
+// const Basket = () => {
+// 	const [isBasketOpen, setIsBasketOpen] = useState<boolean>(false)
+// 	return (
+// 		<>
+// 			{isBasketOpen ? (
+//                 <div>
                     
-                </div>
-			) : (
-				<div>
-					<AiOutlineShoppingCart
-						className={styles.basketBall}
-						color={'white'}
-                            size={'7vh'}
-                            onClick={() => setIsBasketOpen(true)}
-					/>
-				</div>
-			)}
-		</>
-	)
-}
-export default Basket
+//                 </div>
+// 			) : (
+// 				<div>
+// 					<AiOutlineShoppingCart
+// 						className={styles.basketBall}
+// 						color={'white'}
+//                             size={'7vh'}
+//                             onClick={() => setIsBasketOpen(true)}
+// 					/>
+// 				</div>
+// 			)}
+// 		</>
+// 	)
+// }
+// export default Basket
+
+
+
+
+
+import React from 'react';
+
+const ShapeWithImage = () => {
+  return (
+    <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+      {/* Create a custom path for the shape */}
+      <path d="M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80" fill="lightblue" />
+
+      {/* Insert an image inside the shape */}
+      <clipPath id="shapeClip">
+        <path d="M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80" />
+      </clipPath>
+
+      <image x="10" y="10" width="180" height="180" xlinkHref="/images/ali.jpg" clipPath="url(#shapeClip)" />
+    </svg>
+  );
+};
+
+export default ShapeWithImage;
--- pages/CloudyPost/index.jsx
+++ pages/CloudyPost/index.jsx
@@ -11,29 +11,39 @@ const CloudyPost = () => {
 		const svg = d3.select(svgRef.current)
 		const { width, height } = svg.node().getBoundingClientRect()
 		svg.selectAll('*').remove()
-
-		const curveGenerator = () =>
-			d3
-				.line()
-				.x((d) => d.x)
-				.y((d) => d.y)
-				.curve(d3.curveCatmullRom) // Use curveCatmullRom for a smoother connection
-
-		const drawCurve = (curveData, bg, color) => {
-			svg
-				.append('path')
-				.datum(curveData)
-				.transition()
-				.duration(2222)
-				.ease(d3.easeBounceInOut)
-				.attr('fill', bg)
-				.attr('stroke', color)
-				.attr('stroke-width', 4)
-				.attr('d', curveGenerator())
+		const yekta = []
+		for (let i = 0; i < 55; i++) {
+			if (i > 5 && i < 21) {
+				const x = width / 1.65 - (i - 7) * 40
+				const y = i % 2 === 0 ? width / 20 : width / 14
+				yekta.push({ x, y })
+			}
+			//Left
+			if (i > 21 && i < 30) {
+				const x = i % 2 != 0 ? width / 3.3 : width / 3.5
+				const y = width / 24 + (i - 21) * 40
+				yekta.push({ x, y })
+			}
+			//Bottom
+			if (i > 30 && i < 47) {
+				const x = width / 4 + (i - 30) * 40
+				const y = i % 2 != 0 ? width / 4 : width / 3.7
+				yekta.push({ x, y })
+			}
+			if (i > 46 && i < 55) {
+				const x =
+					i === 54
+						? width / 1.59
+						: i % 2 != 0
+						? width / 1.6 + (i - 16)
+						: width / 1.65 + (i - 16)
+				const y = i === 54 ? width / 20 : width / 4 - (i - 46) * 40
+				yekta.push({ x, y })
+			}
 		}
-
 		const data = [
-			{ x: width / 1.62, y: width / 4.7 },
+			//Right-Side
+			{ x: width / 1.6, y: width / 4 },
 			{ x: width / 1.65, y: width / 5 },
 			{ x: width / 1.6, y: width / 5.5 },
 			{ x: width / 1.65, y: width / 6 },
@@ -44,6 +54,7 @@ const CloudyPost = () => {
 			{ x: width / 1.6, y: width / 11 },
 			{ x: width / 1.65, y: width / 11 },
 			{ x: width / 1.6, y: width / 14 },
+			// Top-Side
 			{ x: width / 1.65, y: width / 16 },
 			{ x: width / 1.7, y: width / 26 },
 			{ x: width / 1.8, y: width / 20 },
@@ -55,6 +66,8 @@ const CloudyPost = () => {
 			{ x: width / 2.4, y: width / 22 },
 			{ x: width / 2.5, y: width / 30 },
 			{ x: width / 2.7, y: width / 21 },
+			// Left-Side
+
 			{ x: width / 2.8, y: width / 18 },
 			{ x: width / 2.7, y: width / 16 },
 			{ x: width / 2.8, y: width / 14 },
@@ -68,7 +81,11 @@ const CloudyPost = () => {
 			{ x: width / 2.8, y: width / 6.5 },
 			{ x: width / 2.7, y: width / 6 },
 			{ x: width / 2.8, y: width / 5.5 },
-			{ x: width / 2.7, y: width / 5 },
+
+			// { x: width / 2.7, y: width / 5 },
+
+			//Bottom-Side
+
 			{ x: width / 2.8, y: width / 4.5 },
 			{ x: width / 2.5, y: width / 5 },
 			{ x: width / 2.4, y: width / 4.7 },
@@ -82,10 +99,34 @@ const CloudyPost = () => {
 			{ x: width / 1.7, y: width / 4.3 },
 			{ x: width / 1.68, y: width / 4.8 },
 			{ x: width / 1.6, y: width / 4.4 },
-			{ x: width / 1.62, y: width / 4.7 },
 		]
 
-		drawCurve(data, 'white', 'lightblue')
+		const curveGenerator = () =>
+			d3
+				.line()
+				.x((d) => d.x)
+				.y((d) => d.y)
+				.curve(d3.curveCardinal) // Use curveCatmullRom for a smoother connection
+
+		svg
+			.append('path')
+			.datum(yekta)
+			.attr('d', curveGenerator())
+			.attr('fill', 'url(#imagePattern)') // Apply the image pattern fill
+			.attr('stroke', 'lightblue')
+			.attr('stroke-width', 4)
+
+		svg
+			.append('defs')
+			.append('pattern')
+			.attr('id', 'imagePattern') // Set a unique ID for the pattern
+			.attr('patternUnits', 'userSpaceOnUse')
+			.attr('width', width)
+			.attr('height', height)
+			.append('image')
+			.attr('xlink:href', '/images/ali.jpg') // Replace with your image URL
+			.attr('width', width)
+			.attr('height', height)
 	}, [])
 
 	return (
