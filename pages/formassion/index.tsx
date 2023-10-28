3e556dec4a7f682607061a72f4918d9b51a56e2d
Alireza <103531312+alirezashon@users.noreply.github.com>
2023-10-28 23:03:52 +0330

    Create index.tsx

---
+++ pages/formassion/index.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react"
+import styles from "./index.module.css"
+const Akbaraformassion = () => {
+  const [isEyeOpen, setIsEyeOpen] = useState<boolean>(true)
+  return (
+    <>
+      <div className={styles.AkbaraforBase}>
+        {isEyeOpen ? (
+          <div className={styles.AkbaraforEyes}>
+            <div className={styles.AkbaraforinerEyes}>
+                <div className={styles.Akbaramali}> 
+                    </div>
+            </div>
+
+
+
+
+
+
+
+          </div>
+        ) : (
+          <div className={styles.Akbaraformassion}>
+            <div className={styles.header}>
+              <p className={styles.title}>title</p>
+            </div>
+            <div className={styles.formRow}>
+              <input type='email' className={styles.email}/>
+              <label className={styles.label}>Email</label>
+              <input type='number' className={styles.phone}/>
+              <label className={styles.label}>PhonNumber</label>
+            </div>
+            <div className={styles.formRow}>
+              <input type='password' className={styles.password}/>
+              <label className={styles.label}>Password</label>
+              <input type='number' className={styles.houseNumber}/>
+              <label className={styles.label}>Password</label>
+              <input type='number' className={styles.unit}/>
+              <label className={styles.label}>Password</label>
+              
+              <input type='text' className={styles.zipcode}/>
+              <label className={styles.label}>Zip Code</label>
+              
+
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  )
+}
+export default Akbaraformassion
