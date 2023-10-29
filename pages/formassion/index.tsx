57c054b01ef76b974c583c9df2664d3348d9bdbe
Alireza <103531312+alirezashon@users.noreply.github.com>
2023-10-29 23:31:53 +0330

    Update index.tsx

--- pages/formassion/index.tsx
+++ pages/formassion/index.tsx
@@ -1,23 +1,15 @@
 import { useState } from "react"
 import styles from "./index.module.css"
 const Akbaraformassion = () => {
-  const [isEyeOpen, setIsEyeOpen] = useState<boolean>(true)
+  const [isEyeOpen, setIsEyeOpen] = useState<boolean>(false)
   return (
     <>
       <div className={styles.AkbaraforBase}>
         {isEyeOpen ? (
           <div className={styles.AkbaraforEyes}>
             <div className={styles.AkbaraforinerEyes}>
-                <div className={styles.Akbaramali}> 
-                    </div>
+              <div className={styles.Akbaramali}></div>
             </div>
-
-
-
-
-
-
-
           </div>
         ) : (
           <div className={styles.Akbaraformassion}>
@@ -25,23 +17,28 @@ const Akbaraformassion = () => {
               <p className={styles.title}>title</p>
             </div>
             <div className={styles.formRow}>
-              <input type='email' className={styles.email}/>
+              <input type="email" className={styles.email} />
               <label className={styles.label}>Email</label>
-              <input type='number' className={styles.phone}/>
+            </div>
+            <div className={styles.formRow}>
+              <input type="number" className={styles.phone} />
               <label className={styles.label}>PhonNumber</label>
             </div>
             <div className={styles.formRow}>
-              <input type='password' className={styles.password}/>
+              <input type="password" className={styles.password} />
               <label className={styles.label}>Password</label>
-              <input type='number' className={styles.houseNumber}/>
-              <label className={styles.label}>Password</label>
-              <input type='number' className={styles.unit}/>
-              <label className={styles.label}>Password</label>
-              
-              <input type='text' className={styles.zipcode}/>
+            </div>
+            <div className={styles.formRow}>
+              <input type="number" className={styles.houseNumber} />
+              <label className={styles.label}>House Number</label>
+            </div>
+            <div className={styles.formRow}>
+              <input type="number" className={styles.unit} />
+              <label className={styles.label}>Unit</label>
+              </div>
+              <div className={styles.formRow}>
+              <input type="text" className={styles.zipcode} />
               <label className={styles.label}>Zip Code</label>
-              
-
             </div>
           </div>
         )}
